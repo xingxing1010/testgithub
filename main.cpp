1d8352212028083c7b0f5c7cbc5662ba6c136e45
xingxing1010 <2551861088@qq.com>
2024-03-07 23:14:35 +0800

    vvvvvvvv

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "CCMainWindow.h"
+#include <QtWidgets/QApplication>
+#include"UserLogin.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+   /* CCMainWindow w;
+    w.show();*/
+    a.setQuitOnLastWindowClosed(false);
+    UserLogin* userlogin = new UserLogin;
+    userlogin->show();
+
+    return a.exec();
+}
